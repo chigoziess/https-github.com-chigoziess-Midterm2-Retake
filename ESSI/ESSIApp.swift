19ccd6d1516240cd86659faef6c346a8b3ee9cc9
Chigozie Sumani <sumanic@bc.edu>
2025-04-14 16:32:22 -0400

    Initial Commit

---
+++ ESSI/ESSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESSIApp.swift
+//  ESSI
+//
+//  Created by Chigozie Sumani on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ESSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
